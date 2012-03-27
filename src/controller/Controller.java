c86b11e8aa96cec7c0cd0fea7e1eacb8e13a7707
Adam <irokez_adam@poczta.fm>
2012-03-27 19:14:54 +0200

    zmiana layaout

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -26,144 +26,84 @@ public class Controller extends AbstractAction implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if(e.getActionCommand().equals("GENERATE_KEYS"))
+		{
+			blind.generateRsaKeys();
+			window.setKey1(blind.getN().toString());
+			window.setKey2(blind.getE().toString());
+			window.setKey3(blind.getD().toString());
+		}
 		if (e.getActionCommand().equals("EN")) {
-			// encrypt
-			if (window.getMethod() == 1) {
-				// DESX
-				if (window.getFileOrWindow() == 1) {
-					// File
-					String fileName = window.getFileName();
-				} else {
-					// Window
-				}
-			}
-			if (window.getMethod() == 2) {
-				// Knapsack
-				if (window.getFileOrWindow() == 1) {
-					// File
-					String fileName = window.getFileName();
-				} else {
-					// Window
-				}
-			}
-			if (window.getMethod() == 3) {
-				// RSA
-				
-				
-				if(window.getKey1().isEmpty() && window.getKey2().isEmpty() && window.getKey3().isEmpty())
-				{
-					blind.generateRsaKeys();
-					window.setKey1(blind.getN().toString());
-					window.setKey2(blind.getE().toString());
-					window.setKey3(blind.getD().toString());					
-				}
-				else
-					{
+
 					if(window.getKey1().isEmpty() || window.getKey2().isEmpty() || window.getKey3().isEmpty())
 						JOptionPane.showMessageDialog(window,
 							    "Brakuje klucza",
 							    "Blad",
 							    JOptionPane.ERROR_MESSAGE);
 					else
-					{
-						blind.setN(new BigInteger(window.getKey1()));
-						blind.setE(new BigInteger(window.getKey2()));
-						blind.setD(new BigInteger(window.getKey3()));
+					{				
+							blind.setN(new BigInteger(window.getKey1()));
+							blind.setE(new BigInteger(window.getKey2()));
+							blind.setD(new BigInteger(window.getKey3()));						
 					}
 
 				
-				if (window.getFileOrWindow() == 1) {
-					// File
-					String fileName = window.getFileName();
-					// czytanie pliku
-					FileReader fr;
-					try {
-						fr = new FileReader(fileName);
-						BufferedReader br = new BufferedReader(fr);
-						String s;
+//				if (window.getFileOrWindow() == 1) {
+//					// File
+//					String fileName = window.getFileName();
+//					// czytanie pliku
+//					FileReader fr;
+//					try {
+//						fr = new FileReader(fileName);
+//						BufferedReader br = new BufferedReader(fr);
+//						String s;
+//					
+//						while((s = br.readLine()) != null)
+//						{
+//						window.setInput2(s);
+//						}
+//						fr.close();
+//						
+//						//Kodowanie
+//						
+//						
+//					} catch (FileNotFoundException e1) {
+//						// TODO Auto-generated catch block
+//						e1.printStackTrace();
+//					} catch (IOException e1) {
+//						// TODO Auto-generated catch block
+//						e1.printStackTrace();
+//					}
+//			
+//				} 
 					
-						while((s = br.readLine()) != null)
-						{
-						window.setInput2(s);
-						}
-						fr.close();
-						
-						//Kodowanie
-						
-						
-					} catch (FileNotFoundException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-			
-				} 
 					if(blind.getD()!=null)
 					window.setOutput( blind.return_encrypted_msg( window.getInput() ));
-					else
-					{
-						
-						
-						
-					}
 
-					
-					//Kodowanie
-				
-
-			}
-		} }else {
-			//System.out.println("TU JESTEM!!!!!");
-			// decrypt
-			if (window.getMethod() == 1) {
-				// DESX
-				if (window.getFileOrWindow() == 1) {
-					// File
-					String fileName = window.getFileName();
-				} else {
-					// Window
-				}
-			}
-			if (window.getMethod() == 2) {
-				// Knapsack
-				if (window.getFileOrWindow() == 1) {
-					// File
-					String fileName = window.getFileName();
-				}
-			}
-			if (window.getMethod() == 3) {
-				// RSA
-				
-				window.setOutput( blind.return_decrypted_msg( window.getInput() ));
-
-			}
-			else
-			{
-				if(window.getKey1().isEmpty() && window.getKey2().isEmpty() && window.getKey3().isEmpty())
-				{
-					blind.generateRsaKeys();
-					window.setKey1(blind.getN().toString());
-					window.setKey2(blind.getE().toString());
-					window.setKey3(blind.getD().toString());					
-				}
-				else
-					{
-					if(window.getKey1().isEmpty() || window.getKey2().isEmpty() || window.getKey3().isEmpty())
+			
+		} 
+		
+		if(e.getActionCommand().equals("DE"))
+		{
+			
+			if(window.getKey1().isEmpty() || window.getKey2().isEmpty() || window.getKey3().isEmpty())
 						JOptionPane.showMessageDialog(window,
 							    "Brakuje klucza",
 							    "Blad",
 							    JOptionPane.ERROR_MESSAGE);
-					else
-					{
-						blind.setN(new BigInteger(window.getKey1()));
-						blind.setE(new BigInteger(window.getKey2()));
-						blind.setD(new BigInteger(window.getKey3()));
-					}
-					}
+			else
+			{
+				blind.setN(new BigInteger(window.getKey1()));
+				blind.setE(new BigInteger(window.getKey2()));
+				blind.setD(new BigInteger(window.getKey3()));
 			}
+					
+		
+				window.setOutput( blind.return_decrypted_msg( window.getInput() ));
+
 		}
+
+		
 			
 	}
 
@@ -171,7 +111,8 @@ public class Controller extends AbstractAction implements ActionListener {
 		window = new MainWindow();
 		window.setAll();
 		window.repaint();
-		window.setController(this);
+
+		//window.setController(this);
 	}
 
 }
