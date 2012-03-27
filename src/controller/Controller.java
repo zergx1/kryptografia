e4a5b7a3bf8d4c97f2166640d2342e6b2e25677c
Adam <irokez_adam@poczta.fm>
2012-03-27 22:47:27 +0200

    1 mierwsze w syfiastosci kodu

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,14 +1,18 @@
 package controller;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigInteger;
 
 import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 import layout.MainWindow;
@@ -22,6 +26,10 @@ public class Controller extends AbstractAction implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	private MainWindow window;
 	private Rsa blind=new Rsa();
+	private JFileChooser fc=new JFileChooser();
+	private File file;
+	private boolean winIn=true;
+	private boolean winOut=true;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -33,13 +41,73 @@ public class Controller extends AbstractAction implements ActionListener {
 			window.setKey2(blind.getE().toString());
 			window.setKey3(blind.getD().toString());
 		}
-		if (e.getActionCommand().equals("EN")) {
+		if(e.getActionCommand().equals("FileIn"))
+		{
+			int returnVal = fc.showOpenDialog(window);
 
+		      if (returnVal == JFileChooser.APPROVE_OPTION) {
+		    	  file=fc.getSelectedFile();
+		    	  window.setFileIn(file);
+		    	  window.setLabelFrom("From: "+file.toPath().toString());
+		    	  window.getTextArea().setEditable(false);
+		    	  window.getTextArea().setText("");
+		    	  window.getTextArea().setBackground(Color.LIGHT_GRAY);
+		    	  winIn=false;
+		      } 
+		      else
+		    	  if(winIn==true)
+		    	  window.getRdbtnWindowIn().setSelected(true);
+			
+		}
+		if(e.getActionCommand().equals("FileOut"))
+		{
+			int returnVal = fc.showSaveDialog(window);
+
+		      if (returnVal == JFileChooser.APPROVE_OPTION) {
+		    	  file=fc.getSelectedFile();
+		    	  window.setFileOut(file);
+		    	  window.setLabelTo("To: "+file.toPath().toString());
+		    	  window.getTextArea_1().setEditable(false);
+		    	  window.getTextArea_1().setText("");
+		    	  window.getTextArea_1().setBackground(Color.LIGHT_GRAY);
+		    	  winOut=false;
+		      } 
+		      else
+		      {
+		    	  if(winOut==true)
+		    	  {
+		    	  window.getRdbtWindowsOut().setSelected(true);
+		    	  window.getRdbtFileOut().setSelected(false);
+		    	  }
+		      }
+		}
+		if(e.getActionCommand().equals("WindowIn"))
+		{
+			window.setLabelFrom("");
+	    	  window.getTextArea().setEditable(true);
+	    	  window.getTextArea().setBackground(Color.white);
+	    	  winIn=true;
+		}
+		if(e.getActionCommand().equals("WindowOut"))
+		{
+			window.setLabelTo("");
+	    	  window.getTextArea_1().setEditable(true);
+	    	  window.getTextArea_1().setBackground(Color.white);
+	    	  winOut=true;
+		}
+		
+		
+		
+		if (e.getActionCommand().equals("EN")) {
+			System.out.println("ENCRYPTED");
 					if(window.getKey1().isEmpty() || window.getKey2().isEmpty() || window.getKey3().isEmpty())
+					{
 						JOptionPane.showMessageDialog(window,
 							    "Brakuje klucza",
 							    "Blad",
 							    JOptionPane.ERROR_MESSAGE);
+						return;
+					}
 					else
 					{				
 							blind.setN(new BigInteger(window.getKey1()));
@@ -47,50 +115,99 @@ public class Controller extends AbstractAction implements ActionListener {
 							blind.setD(new BigInteger(window.getKey3()));						
 					}
 
-				
-//				if (window.getFileOrWindow() == 1) {
-//					// File
-//					String fileName = window.getFileName();
-//					// czytanie pliku
-//					FileReader fr;
-//					try {
-//						fr = new FileReader(fileName);
-//						BufferedReader br = new BufferedReader(fr);
-//						String s;
-//					
-//						while((s = br.readLine()) != null)
-//						{
-//						window.setInput2(s);
-//						}
-//						fr.close();
-//						
-//						//Kodowanie
-//						
-//						
-//					} catch (FileNotFoundException e1) {
-//						// TODO Auto-generated catch block
-//						e1.printStackTrace();
-//					} catch (IOException e1) {
-//						// TODO Auto-generated catch block
-//						e1.printStackTrace();
-//					}
-//			
-//				} 
-					
-					if(blind.getD()!=null)
-					window.setOutput( blind.return_encrypted_msg( window.getInput() ));
+					if(winIn==true)
+					{
+						//z pola tekstowego
+						if(winOut==true)
+						{
+							//do pola tekstowego
+							window.setOutput( blind.return_encrypted_msg( window.getInput() ));
+						}
+						else
+						{
+							//do pliku
+							FileOutputStream fos;
+							try {
+								fos = new FileOutputStream(window.getFileOut());	
+					        try {
+					        	String en = blind.return_encrypted_msg( window.getInput() );
+					            //en = blind.return_decrypted_msg(en);
+								fos.write(en.getBytes());
+						        fos.flush();
+						        fos.close();
+							} catch (IOException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+					        } catch (FileNotFoundException e2) {
+								// TODO Auto-generated catch block
+								e2.printStackTrace();
+							}
+						}
+					}
+					else
+					{
+						//z pliku
+						try {
+							byte[] by = blind.fileToBytes(window.getFileIn());
+							System.out.println(window.getFileIn());
+							try
+							{
+								System.out.println("Z pliku encrypt");
+								BigInteger bigby = new BigInteger( by );
+								by = bigby.toByteArray();
+							    String en = blind.return_encrypted_msg( by );
+								if(winOut==true)
+								{
+								//do pola tekstowego
+								window.setOutput(en);
+								}
+								else
+								{
+									//do pliku
 
-			
+							        FileOutputStream fos = new FileOutputStream(window.getFileOut());
+							        try {
+										fos.write(en.getBytes());
+								        fos.flush();
+								        fos.close();
+									} catch (IOException e1) {
+										// TODO Auto-generated catch block
+										e1.printStackTrace();
+									}
+								}
+							}
+							catch(java.lang.NumberFormatException e1)
+							{
+								JOptionPane.showMessageDialog(window,
+									    "Plik jest pusty",
+									    "Blad",
+									    JOptionPane.ERROR_MESSAGE);
+							}
+							
+							
+						} catch (FileNotFoundException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					        
+						
+						
+					}
+		
 		} 
 		
 		if(e.getActionCommand().equals("DE"))
 		{
-			
+			System.out.println("DECRYPTED");
 			if(window.getKey1().isEmpty() || window.getKey2().isEmpty() || window.getKey3().isEmpty())
+			{
 						JOptionPane.showMessageDialog(window,
 							    "Brakuje klucza",
 							    "Blad",
 							    JOptionPane.ERROR_MESSAGE);
+						return;
+			}
 			else
 			{
 				blind.setN(new BigInteger(window.getKey1()));
@@ -99,19 +216,163 @@ public class Controller extends AbstractAction implements ActionListener {
 			}
 					
 		
-				window.setOutput( blind.return_decrypted_msg( window.getInput() ));
+				if(winIn==true)
+				{
+					//z pola tekstowego
+					if(winOut==true)
+					{
+						//do pola tekstowego
+						window.setOutput( blind.return_decrypted_msg( window.getInput() ));
+					}
+					else
+					{
+						//do pliku
+						FileOutputStream fos;
+						try {
+							fos = new FileOutputStream(window.getFileOut());	
+				        try {
+				        	String en =window.getInput();
+				            en = blind.return_decrypted_msg(en);
+							fos.write(en.getBytes());
+					        fos.flush();
+					        fos.close();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+				        } catch (FileNotFoundException e2) {
+							// TODO Auto-generated catch block
+							e2.printStackTrace();
+						}
+					}
+				}
+				else
+				{
+					//z pliku
+					try {
+						//byte[] by = blind.fileToBytes(window.getFileIn());
+						System.out.println(window.getFileIn());
+						FileReader fr = new FileReader(window.getFileIn());
+						BufferedReader br = new BufferedReader(fr);
+						String s,en="";
+						
+						try {
+							while((s = br.readLine()) != null)
+							{
+							en+=s;
+							}
+							
+							//fr.close();
+							try
+							{
+								System.out.println("ENCRYPTED z pliku");
+								//BigInteger bigby = new BigInteger( by );
+								//by = bigby.toByteArray();
+							    en=blind.return_decrypted_msg(en);
+								if(winOut==true)
+								{
+								//do pola tekstowego
+									System.out.println("Z pliku na konsole");
+								window.setOutput(en);
+								}
+								else
+								{
+									//do pliku
+									System.out.println("Z pliku do pliku");
+							        FileOutputStream fos = new FileOutputStream(window.getFileOut());
+							        try {
+										fos.write(en.getBytes());
+								        fos.flush();
+								        fos.close();
+									} catch (IOException e1) {
+										// TODO Auto-generated catch block
+										e1.printStackTrace();
+									}
+								}
+							
+						} catch (IOException e2) {
+							// TODO Auto-generated catch block
+							e2.printStackTrace();
+						}
+						
+						
+					}
+						catch(java.lang.NumberFormatException e1)
+						{
+							//z pliku
+							try {
+								byte[] by = blind.fileToBytes(window.getFileIn());
+								System.out.println(window.getFileIn());
+								try
+								{
+									System.out.println("Z pliku encrypt");
+									BigInteger bigby = new BigInteger( by );
+
+								    en = blind.return_decrypted_msg( bigby.toString() );
+									if(winOut==true)
+									{
+									//do pola tekstowego
+									window.setOutput(en);
+									}
+									else
+									{
+										//do pliku
+
+								        FileOutputStream fos = new FileOutputStream(window.getFileOut());
+								        try {
+											fos.write(en.getBytes());
+									        fos.flush();
+									        fos.close();
+										} catch (IOException e11) {
+											// TODO Auto-generated catch block
+											e11.printStackTrace();
+										}
+									}
+								}
+								catch(java.lang.NumberFormatException e11)
+								{
+									JOptionPane.showMessageDialog(window,
+										    "Plik jest pusty",
+										    "Blad",
+										    JOptionPane.ERROR_MESSAGE);
+								}
+								
+								
+							} catch (FileNotFoundException e11) {
+								// TODO Auto-generated catch block
+								e11.printStackTrace();
+							}
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						
+						
+					}catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				        
+					
+					
+				
+	
 
 		}
 
 		
 			
 	}
-
+		}
 	public Controller() {
 		window = new MainWindow();
+		
 		window.setAll();
 		window.repaint();
+		window.setVisible(true);
 		window.setController(this);
+		window.getRdbtnWindowIn().setSelected(true);
+		window.getRdbtWindowsOut().setSelected(true);
 		
 	}
 
--- src/layout/MainWindow.java
+++ src/layout/MainWindow.java
@@ -18,12 +18,11 @@ import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.JLabel;
 
 public class MainWindow extends JFrame {
 
@@ -36,18 +35,23 @@ public class MainWindow extends JFrame {
 	private final ButtonGroup buttonGroup_1 = new ButtonGroup();
 	private JTextArea textArea;
 	private JTextArea textArea_1;
-	private JTextField textField;
-	private JRadioButton rdbtnFile;
-	private JPanel panel_4;
+	private JRadioButton rdbtnFileIn;
 	private JPanel panel_5;
 	private JButton btnEncrypt;
 	private JButton btnDecrypt;
+	private JButton btnGenerateKeys;
 	private JTextField key1;
 	private JTextField key2;
 	private JTextField key3;
-	private final ButtonGroup buttonGroup_2 = new ButtonGroup();
-	private JRadioButton rdbtnYes;
-
+	private File fileIn;
+	private File fileOut;
+	private JRadioButton rdbtFileOut;
+	private JRadioButton rdbtWindowsOut;
+	private JRadioButton rdbtnWindowIn;
+	private JLabel labelFrom;
+	private JLabel labelTo;
+	
+	
 	public MainWindow() {
 		setAll();
 	}
@@ -69,11 +73,12 @@ public class MainWindow extends JFrame {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		setTitle("Cryptography");
+		setTitle("Cryptography, blind signature with RSA");
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 622);
 		contentPane = new JPanel();
+		contentPane.setToolTipText("Generate keys");
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -117,94 +122,64 @@ public class MainWindow extends JFrame {
 		scrollPane_1.setViewportView(textArea_1);
 
 		btnEncrypt = new JButton("Encrypt");
+		btnEncrypt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		btnEncrypt.setActionCommand("EN");
-		btnEncrypt.setBounds(341, 358, 91, 42);
+		btnEncrypt.setBounds(341, 358, 83, 33);
 		contentPane.add(btnEncrypt);
 
 		btnDecrypt = new JButton("Decrypt");
+		btnDecrypt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+
 		btnDecrypt.setActionCommand("DE");
-		btnDecrypt.setBounds(341, 411, 91, 42);
+		btnDecrypt.setBounds(341, 402, 83, 33);
 		contentPane.add(btnDecrypt);
-
-		JPanel panel_3 = new JPanel();
-		panel_3.setBorder(new TitledBorder(new EtchedBorder(
-				EtchedBorder.LOWERED, null, null), "Method",
-				TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_3.setBounds(10, 359, 121, 125);
-		contentPane.add(panel_3);
-		panel_3.setLayout(null);
-
-		JRadioButton rdbtnDesx = new JRadioButton("DESX", true);
-		rdbtnDesx.setActionCommand("A");
-		rdbtnDesx.setBounds(6, 16, 109, 23);
-		panel_3.add(rdbtnDesx);
-		buttonGroup.add(rdbtnDesx);
-
-		JRadioButton rdbtnKnapsack = new JRadioButton("Knapsack");
-		rdbtnKnapsack.setActionCommand("B");
-		rdbtnKnapsack.setBounds(6, 56, 109, 23);
-		panel_3.add(rdbtnKnapsack);
-		buttonGroup.add(rdbtnKnapsack);
-
-		JRadioButton rdbtnThird = new JRadioButton("RSA");
-
-		rdbtnThird.setActionCommand("C");
-		rdbtnThird.setBounds(6, 95, 109, 23);
-		panel_3.add(rdbtnThird);
-		buttonGroup.add(rdbtnThird);
+		
+		btnGenerateKeys = new JButton("Keys");
+		btnGenerateKeys.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnGenerateKeys.setActionCommand("GENERATE_KEYS");
+		btnGenerateKeys.setBounds(341, 446, 83, 33);
+		contentPane.add(btnGenerateKeys);
 
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new TitledBorder(new EtchedBorder(
-				EtchedBorder.LOWERED, null, null), "Input/Output",
+				EtchedBorder.LOWERED, null, null), "Input",
 				TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_2.setBounds(141, 358, 121, 68);
+		panel_2.setBounds(10, 358, 150, 68);
 		contentPane.add(panel_2);
 		panel_2.setLayout(null);
 
-		rdbtnFile = new JRadioButton("File");
-		rdbtnFile.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent arg0) {
-				if (rdbtnFile.isSelected())
-					panel_4.setVisible(true);
-				else
-					panel_4.setVisible(false);
+		rdbtnFileIn = new JRadioButton("File");
 
-			}
-		});
-		rdbtnFile.setBounds(6, 16, 109, 23);
-		rdbtnFile.setActionCommand("D");
-		panel_2.add(rdbtnFile);
-		buttonGroup_1.add(rdbtnFile);
+		rdbtnFileIn.setBounds(6, 16, 109, 23);
+		rdbtnFileIn.setActionCommand("FileIn");
+		panel_2.add(rdbtnFileIn);
+		buttonGroup_1.add(rdbtnFileIn);
 
-		JRadioButton rdbtnWindow = new JRadioButton("Window", true);
-		rdbtnWindow.setActionCommand("E");
-		rdbtnWindow.setBounds(6, 39, 109, 23);
-		panel_2.add(rdbtnWindow);
-		buttonGroup_1.add(rdbtnWindow);
-
-		panel_4 = new JPanel();
-		panel_4.setBorder(new TitledBorder(null, "File Name",
-				TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_4.setBounds(141, 436, 182, 48);
-		contentPane.add(panel_4);
-		panel_4.setLayout(null);
-		panel_4.setVisible(false);
-
-		textField = new JTextField();
-		textField.setBounds(10, 17, 162, 20);
-		panel_4.add(textField);
-		textField.setColumns(10);
+		rdbtnWindowIn = new JRadioButton("Window", true);
+		rdbtnWindowIn.setBounds(6, 39, 109, 23);
+		rdbtnWindowIn.setActionCommand("WindowIn");
+		panel_2.add(rdbtnWindowIn);
+		buttonGroup_1.add(rdbtnWindowIn);
 
 		panel_5 = new JPanel();
-		panel_5.setBorder(new TitledBorder(null, "Public keys",
+		panel_5.setBorder(new TitledBorder(null, "Keys",
 				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_5.setBounds(10, 487, 422, 85);
 		contentPane.add(panel_5);
 		panel_5.setLayout(null);
-		panel_5.setVisible(false);
+
 
 		JPanel panel_6 = new JPanel();
-		panel_6.setBorder(new TitledBorder(null, "key 1", TitledBorder.LEADING,
+		panel_6.setBorder(new TitledBorder(null, "key N", TitledBorder.LEADING,
 				TitledBorder.TOP, null, null));
 		panel_6.setBounds(10, 24, 132, 52);
 		panel_5.add(panel_6);
@@ -216,7 +191,7 @@ public class MainWindow extends JFrame {
 		key1.setColumns(10);
 
 		JPanel panel_7 = new JPanel();
-		panel_7.setBorder(new TitledBorder(null, "key 2", TitledBorder.LEADING,
+		panel_7.setBorder(new TitledBorder(null, "key E", TitledBorder.LEADING,
 				TitledBorder.TOP, null, null));
 		panel_7.setBounds(150, 24, 134, 53);
 		panel_5.add(panel_7);
@@ -228,7 +203,7 @@ public class MainWindow extends JFrame {
 		key2.setColumns(10);
 
 		JPanel panel_8 = new JPanel();
-		panel_8.setBorder(new TitledBorder(null, "key 3", TitledBorder.LEADING,
+		panel_8.setBorder(new TitledBorder(null, "key D", TitledBorder.LEADING,
 				TitledBorder.TOP, null, null));
 		panel_8.setBounds(294, 24, 118, 53);
 		panel_5.add(panel_8);
@@ -238,33 +213,35 @@ public class MainWindow extends JFrame {
 		key3.setBounds(10, 22, 98, 20);
 		panel_8.add(key3);
 		key3.setColumns(10);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new TitledBorder(new EtchedBorder(
+				EtchedBorder.LOWERED, null, null), "Output",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_3.setLayout(null);
+		panel_3.setBounds(170, 358, 150, 68);
+		contentPane.add(panel_3);
+		
+		rdbtFileOut = new JRadioButton("File");
+		rdbtFileOut.setActionCommand("FileOut");
+		rdbtFileOut.setBounds(6, 16, 109, 23);
+		panel_3.add(rdbtFileOut);
+		buttonGroup.add(rdbtFileOut);
+		
+		rdbtWindowsOut = new JRadioButton("Window", true);
+		rdbtWindowsOut.setBounds(6, 39, 109, 23);
+		rdbtWindowsOut.setActionCommand("WindowOut");
+		panel_3.add(rdbtWindowsOut);
+		buttonGroup.add(rdbtWindowsOut);
 
-		JPanel panel_9 = new JPanel();
-		panel_9.setBorder(new TitledBorder(null, "Key", TitledBorder.LEADING,
-				TitledBorder.TOP, null, null));
-		panel_9.setBounds(272, 358, 59, 78);
-		contentPane.add(panel_9);
-		panel_9.setLayout(null);
 
-		JRadioButton rdbtnNo = new JRadioButton("no");
-		rdbtnNo.setSelected(true);
-		buttonGroup_2.add(rdbtnNo);
-		rdbtnNo.setBounds(8, 23, 37, 23);
-		panel_9.add(rdbtnNo);
-
-		rdbtnYes = new JRadioButton("yes");
-		rdbtnYes.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent arg0) {
-				if (rdbtnYes.isSelected())
-					panel_5.setVisible(true);
-				else
-					panel_5.setVisible(false);
-
-			}
-		});
-		buttonGroup_2.add(rdbtnYes);
-		rdbtnYes.setBounds(8, 49, 43, 23);
-		panel_9.add(rdbtnYes);
+		labelFrom = new JLabel("");
+		labelFrom.setBounds(10, 437, 310, 14);
+		contentPane.add(labelFrom);
+		
+		labelTo = new JLabel("");
+		labelTo.setBounds(10, 462, 310, 14);
+		contentPane.add(labelTo);
 
 	}
 
@@ -288,33 +265,19 @@ public class MainWindow extends JFrame {
 		textArea_1.setText(in);
 	}
 
-	public int getMethod() {
-		if (buttonGroup.getSelection().getActionCommand().equals("A"))
-			return 1;// DESX
-		if (buttonGroup.getSelection().getActionCommand().equals("B"))
-			return 2;// Knapsack
-		else
-			return 3;// AA
-	}
+//	public int getFileOrWindowIn() {
+//		if (buttonGroup_1.getSelection().getActionCommand().equals("D"))
+//			return 1;// Plik
+//		else
+//			return 2;// Okno
+//	}
+//	public int getFileOrWindowOut() {
+//		if (buttonGroup.getSelection().getActionCommand().equals("D"))
+//			return 1;// Plik
+//		else
+//			return 2;// Okno
+//	}
 
-	public int getFileOrWindow() {
-		if (buttonGroup_1.getSelection().getActionCommand().equals("D"))
-			return 1;// Plik
-		else
-			return 2;// Okno
-	}
-
-	public String getFileName() {
-		if (getFileOrWindow() == 1)
-			return textField.getText();
-		else
-			return null;
-	}
-
-	public void setController(AbstractAction a) {
-		btnDecrypt.addActionListener(a);
-		btnEncrypt.addActionListener(a);
-	}
 	public String getKey1() {
 		return key1.getText();
 	}
@@ -337,5 +300,65 @@ public class MainWindow extends JFrame {
 	public void setKey3(String key3) {
 		this.key3.setText(key3);
 	}
+	public void setController(AbstractAction a) {
+		btnDecrypt.addActionListener(a);
+		btnEncrypt.addActionListener(a);
+		btnGenerateKeys.addActionListener(a);
+		rdbtFileOut.addActionListener(a);
+		rdbtnFileIn.addActionListener(a);
+		rdbtWindowsOut.addActionListener(a);
+		rdbtnWindowIn.addActionListener(a);
+		//cos
+	}
 
+	public JRadioButton getRdbtFileOut() {
+		return rdbtFileOut;
+	}
+	public JRadioButton getRdbtnWindowIn() {
+		return rdbtnWindowIn;
+	}
+	public JRadioButton getRdbtnFileIn() {
+		return rdbtnFileIn;
+	}
+	public JRadioButton getRdbtWindowsOut() {
+		return rdbtWindowsOut;
+	}
+
+	public File getFileIn() {
+		return fileIn;
+	}
+
+	public void setFileIn(File fileIn) {
+		this.fileIn = fileIn;
+	}
+
+	public File getFileOut() {
+		return fileOut;
+	}
+
+	public void setFileOut(File fileOut) {
+		this.fileOut = fileOut;
+	}
+	public JLabel getLabelFrom() {
+		return labelFrom;
+	}
+	public JLabel getLabelTo() {
+		return labelTo;
+	}
+
+	public void setLabelFrom(String labelFrom) {
+		this.labelFrom.setText(labelFrom);
+	}
+
+	public JTextArea getTextArea() {
+		return textArea;
+	}
+
+	public JTextArea getTextArea_1() {
+		return textArea_1;
+	}
+
+	public void setLabelTo(String labelTo) {
+		this.labelTo.setText(labelTo);
+	}
 }
